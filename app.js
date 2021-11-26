b587671748b356e892cb67a0a9dc20640cf2e307
Juan Perez <juandp2294@gmail.com>
2021-11-26 14:24:08 -0500

    Second

---
+++ app.js
@@ -0,0 +1,25 @@
+const { mostrarMenu, pausa } = require('./helpers/mensajes');
+
+require('colors')
+console.clear()
+
+
+const main = async() => {
+    console.log('hola');
+
+    let opt = '';
+
+    do{
+        opt = await mostrarMenu();
+        console.log({opt});
+        if(opt !== '0'){
+            await pausa();
+
+        }
+
+    } while(opt !== '0') {
+    }
+
+}
+
+main();
